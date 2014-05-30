fc9485b1f8472988190fa6e07f377e3835a6c517
Chris Haines <hainesc@live.co.uk>
2014-05-30 22:29:05 +0100

    There is a very small possibility that the weak reference could be set to null between checking it for null and checking IsAlive, take a copy first

--- src/MemBroker/WeakActionBase.cs
+++ src/MemBroker/WeakActionBase.cs
@@ -4,7 +4,7 @@ namespace MemBroker
 {
     internal abstract class WeakActionBase
     {
-        protected WeakReference reference;
+        private WeakReference reference;
 
         public Func<object, bool> Filter { get; protected set; }
         
@@ -15,12 +15,20 @@ namespace MemBroker
 
         public bool IsAlive
         {
-            get { return reference != null && reference.IsAlive; }
+            get
+            {
+                var weakReference = reference;
+                return weakReference != null && weakReference.IsAlive;
+            }
         }
 
         public object Target
         {
-            get { return reference != null ? reference.Target : null; }
+            get
+            {
+                var weakReference = reference;
+                return weakReference != null ? weakReference.Target : null;
+            }
         }
 
         public void MarkForDeletion()
